491e4de7a0392ad33290d09373de7ef5b2e946c5
ing-arriola <ingeniero.arriola@gmail.com>
2020-12-27 16:33:07 -0600

    steps from 15 to 22 completed

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,6 +1,9 @@
 //TODO: Step 6 - import the story.dart file into this file.
 import 'story.dart';
 
+//TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
+int storyNumber = 0;
+
 //TODO: Step 5 - Create a new class called StoryBrain.
 class StoryBrain {
 //TODO: Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.
@@ -56,9 +59,25 @@ class StoryBrain {
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
-//TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
-
 //TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
+  void nextStory(int choiceNumber) {
+    if (choiceNumber == 1 && storyNumber == 0) {
+      storyNumber = 2;
+    } else if (choiceNumber == 2 && storyNumber == 0) {
+      storyNumber = 1;
+    } else if (storyNumber == 2 && choiceNumber == 1) {
+      storyNumber = 5;
+    } else if (storyNumber == 2 && choiceNumber == 2) {
+      storyNumber = 4;
+    } else if (storyNumber == 1 && choiceNumber == 1) {
+      storyNumber = 2;
+    } else if (storyNumber == 1 && choiceNumber == 2) {
+      storyNumber = 3;
+    }
+    if (storyNumber == 3 || storyNumber == 4 || storyNumber == 5) {
+      storyNumber = 0;
+    }
+  }
 
 //TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
 
